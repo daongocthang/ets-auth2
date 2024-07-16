ff5371089f8e8ec70e7ea2458c28f540c655561d
daongocthang <daongocthang.vn@gmail.com>
2024-07-17 06:56:08 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express, { Application } from "express";
-import { errorHandler } from "./mildwares/error.mildware";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { Application } from 'express';
+import authenticate from './mildwares/auth.mildware';
+import { errorHandler } from './mildwares/error.mildware';
+import userRouter from './routes/user.routes';
 dotenv.config();
 
 type User = {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 };
 
 declare global {
-  namespace Express {
-    interface Request {
-      user?: User | null;
+    namespace Express {
+        interface Request {
+            user?: User | null;
+        }
     }
-  }
 }
 
 const app: Application = express();
@@ -22,10 +25,14 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(cookieParser());
+
 const port: number = parseInt(process.env.NODE_PORT as string) || 3000;
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
+
+app.use('/users', authenticate, userRouter);
 
 // Catch
 app.use(errorHandler);
